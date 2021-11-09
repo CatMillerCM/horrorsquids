6b7747fc44fb38a8241f738b17bb0e2c3791fef3
Jacken <mboy@hotmail.co.uk>
2021-11-09 15:49:19 +0000

    finishes the code base

--- playedgame.js
+++ playedgame.js
@@ -9,40 +9,66 @@ console.log(lastName);
 console.log(ageGroup);
 console.log(ability);
 
-// <25
-// dexterity - tug of war
-// cunning - stepping stones
-// Speed - honeycomb
-// strength - red light green light
-// Memory - recruiter round
+const welcome = document.getElementById("welcome");
+const playerNumber = document.getElementById("playerNumber");
+const text = document.getElementById("game");
+const number = Math.floor(Math.random() * 456);
+const elimination = document.getElementById("elimination");
 
-// 26-29
-// dexterity - marbles
-// cunning - honeycomb
-// Speed - stepping stones
-// strength - recruiter round
-// Memory - red light green light
-// 30-34
-// dexterity - WINNER
-// cunning - recruiter round
-// Speed - tug of war
-// strength - stepping stones
-// Memory - marbles
-// 35-39
-// dexterity - stepping stones
-// cunning - red light green light
-// Speed - marbles
-// strength - honeycomb
-// Memory - tug of war
-// 40-44
-// dexterity - red light green light
-// cunning - recruiter round
-// Speed - tug of war
-// strength - marbles
-// Memory - honeycomb
-// 45+
-// dexterity - marbles
-// cunning - tug of war
-// Speed - stepping stones
-// strength - recruiter round
-// Memory - honeycomb
+welcome.innerText += ` ${firstName} ${lastName}.`;
+playerNumber.innerText += ` ${number}.`;
+
+const gameObj = {
+  "<25": {
+    Dexterity: "tug of war",
+    Cunning: "glass stepping stones",
+    Speed: "honeycomb",
+    Strength: "red light green light",
+    Memory: "recruiter round",
+  },
+  "26-29": {
+    Dexterity: "marbles",
+    Cunning: "honeycomb",
+    Speed: "stepping stones",
+    Strength: "recruiter round",
+    Memory: "red light green light",
+  },
+  "30-34": {
+    Dexterity: "WINNER",
+    Cunning: "recruiter round",
+    Speed: "tug of war",
+    Strength: "stepping stones",
+    Memory: "marbles",
+  },
+  "35-39": {
+    Dexterity: "stepping stones",
+    Cunning: "red light green light",
+    Speed: "marbles",
+    Strength: "honeycomb",
+    Memory: "tug of war",
+  },
+  "40-44": {
+    Dexterity: "red light green light",
+    Cunning: "recruiter round",
+    Speed: "tug of war",
+    Strength: "marbles",
+    Memory: "honeycomb",
+  },
+  "45+": {
+    Dexterity: "marbles",
+    Cunning: "tug of war",
+    Speed: "glass stepping stones",
+    Strength: "recruiter round",
+    Memory: "honeycomb",
+  },
+};
+
+const outcome = gameObj[ageGroup][ability];
+
+if (outcome === "WINNER") {
+  elimination.innerText = `Congratulations player ${number}, you have completed the games.\n
+  Your total winnings are 45.6 billion won.`;
+} else {
+  elimination.innerText = `Player ${number} eliminated. \n
+  You failed to complete ${outcome}.`;
+}
